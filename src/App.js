595f8a9564dc84feb1a7184735bb1d65a04bbaca
trome23 <romennoodles23@yahoo.com>
2019-06-22 11:12:52 -0600

    images displayed with their name/occupation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import ImageCard from './components/ImageCard'
+import superHeros from './superHeros.json'
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    superHeros
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <Title>SuperHero List</Title>
+        {this.state.superHeros.map(friend => (
+          <ImageCard
+            removeFriend={this.removeFriend}
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+            occupation={friend.occupation}
+            location={friend.location}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+  
 }
 
 export default App;
