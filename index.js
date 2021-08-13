dde72f064eb51f65a93898187c18f4457dcfb130
Tyson Romero <romennoodles23@yahoo.com>
2021-08-13 15:29:08 -0600

    displays hardcoded emoji array at beginning, then can add new emojis at beginning or end, and all displayed in DOM

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const myEmojis = ["👨‍💻", "⛷", "🍲"]
 const emojiContainer = document.querySelector("#emoji-container")
+const pushBtn = document.querySelector("#push-btn")
+const emojiInput = document.querySelector("#emoji-input")
+const unshiftBtn = document.querySelector("#unshift-btn")
 
-for (let i = 0; i < myEmojis.length; i++) {
+let emojiList = ""
+    for (let i = 0; i < myEmojis.length; i++) {
+        emojiList += `<span id = "emoji-span">${myEmojis[i]}</span>`
+    }
+    emojiContainer.innerHTML = emojiList
+
+function render() {
     let emojiList = ""
     for (let i = 0; i < myEmojis.length; i++) {
-        // console.log(myEmojis[i])
         emojiList += `<span id = "emoji-span">${myEmojis[i]}</span>`
     }
     emojiContainer.innerHTML = emojiList
 }
+
+
+pushBtn.addEventListener('click', function() {
+    if (emojiInput.value) {
+        myEmojis.push(emojiInput.value)
+        emojiInput.value = "" 
+        emojiContainer.innerHTML = ""
+        render()
+    } 
+})
+
+unshiftBtn.addEventListener('click', function() {
+    if (emojiInput.value) {
+        myEmojis.unshift(emojiInput.value)
+        emojiInput.value = "" 
+        emojiContainer.innerHTML = ""
+        render()
+    } 
+})
